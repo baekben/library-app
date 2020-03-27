424f04fec91e4fc3fdfb685b8b5f34a948a45b23
Ben <ben10baek@hotmail.com>
2020-03-26 18:03:30 -0700

    Added function to form to add books to Library

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ let myLibrary = [];
 
 let library = document.getElementById('library');
 
+var form = document.getElementById('popupForm');
+var newBook = document.getElementById('addBook');
+var span = document.getElementsByClassName('form');
+
+newBook.onclick = function() {
+  console.log('Form opened');
+  form.style.display = 'block';
+};
+
+span.onclick = function() {
+  form.style.display = 'none';
+};
+
+window.onclick = function(event) {
+  if (event.target == form) {
+    this.form.style.display = 'none';
+  }
+};
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -14,12 +33,18 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
   //do stuff here
-  console.log('Adding Book');
-  title = document.getElementById('title').value;
-  console.log(title);
-  author = document.getElementById('author').value;
-  pages = document.getElementById('pages').value;
-  read = document.getElementById('readStatus').checked;
+  form.style.display = 'none';
+  console.log('Form closed');
+  console.log('Adding Book ...');
+  var title = document.getElementById('title').value;
+  var author = document.getElementById('author').value;
+  var pages = document.getElementById('pages').value;
+  var read = document.getElementById('readStatus').checked;
+  if (read == true) {
+    read = 'Read';
+  } else {
+    read = 'Not Read';
+  }
   if (title == '' || author == '' || pages == 0) {
     console.log('Error');
     return;
@@ -40,26 +65,7 @@ function render() {
     library.appendChild(book);
     //print it to html
   }
+  console.log(myLibrary);
 }
-var form = document.getElementById('popupForm');
-var newBook = document.getElementById('addBook');
-var submit = document.getElementById('submit');
-var span = document.getElementsByClassName('form')[0];
 
-newBook.onclick = function() {
-  form.style.display = 'block';
-};
-
-span.onclick = function() {
-  form.style.display = 'none';
-};
-
-submit.onclick = function() {
-  form.style.display = 'none';
-};
-
-window.onclick = function(event) {
-  if (event.target == form) {
-    this.form.style.display = 'none';
-  }
-};
+function catalog() {}
