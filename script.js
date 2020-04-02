cbb15fbb07bbeaef16f876e2654fd65adbdf8ee2
Ben <ben10baek@hotmail.com>
2020-04-02 15:09:32 -0700

    Added feature to change read status on table

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ var span = document.getElementsByClassName('form');
 
 var idNumber = 0;
 
+const bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'No');
+const booktwo = new Book('Meditations', 'Marcus Aurelius', '200', 'Yes');
+const bookthree = new Book(
+  'Thinking Fast and Slow',
+  'Daniel Kahneman',
+  '400',
+  'No'
+);
+
+addBookToLibrary(bookOne);
+addBookToLibrary(booktwo);
+addBookToLibrary(bookthree);
+
 newBook.onclick = function() {
   document.getElementById('title').value = null;
   document.getElementById('author').value = null;
@@ -32,13 +45,9 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  // this.info = function() {
-  //   return [title, author, pages, read];
-  // };
 }
 
-function addBookToLibrary() {
-  //do stuff here
+function createNewBook() {
   form.style.display = 'none';
   console.log('Form closed');
   console.log('Adding Book ...');
@@ -47,55 +56,76 @@ function addBookToLibrary() {
   var pages = document.getElementById('pages').value;
   var read = document.getElementById('readStatus').checked;
   if (read == true) {
-    read = 'Read';
+    read = 'Yes';
   } else {
-    read = 'Not Read';
+    read = 'No';
   }
   if (title == '' || author == '' || pages == 0) {
     console.log('Error');
     return;
   }
-
+  //create new book
   let book = new Book();
   book.title = title;
   book.author = author;
   book.pages = pages;
   book.read = read;
-  myLibrary.push(book);
+  //add book to temp array
+  addBookToLibrary(book);
+  //tempLibrary.push(book);
 }
 
-function render() {
-  if (myLibrary == '') {
+function addBookToLibrary(book) {
+  myLibrary.push(book); //pushing new book to library
+  render(myLibrary);
+}
+
+function render(library = myLibrary) {
+  if (library == '') {
     alert('ERROR\nPlease add a book before\nrendering.');
   }
-  console.log(myLibrary);
   console.log('Rendering...');
-  let table = document.querySelector('table');
-  let library = Object.keys(myLibrary[0]);
-  makeTable(table, myLibrary);
-  // generateTableHead(table, library);
-  console.log(myLibrary);
+  let table = document.querySelector('tbody');
+  while (table.rows.length >= 1) {
+    table.deleteRow(0);
+  }
+  makeTable(table, library);
 }
-
-// function generateTableHead(table, data) {
-//   let thead = table.createTHead();
-//   let row = thead.insertRow();
-//   for (let key of data) {
-//     let th = document.createElement('th');
-//     let text = document.createTextNode(key);
-//     th.appendChild(text);
-//     row.appendChild(th);
-//   }
-// }
 
 function makeTable(table, library) {
   for (let book of library) {
     let row = table.insertRow();
-    row.setAttribute('id', idNumber++);
+    row.setAttribute('id', `row${idNumber}`);
     for (key in book) {
       let cell = row.insertCell();
+      if (key == 'read') {
+        cell.setAttribute('id', `read${idNumber}`);
+        cell.setAttribute('onclick', 'status(this.id)');
+      }
       let text = document.createTextNode(book[key]);
       cell.appendChild(text);
     }
+    let deleteFeature = row.insertCell(4);
+    var deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete Book';
+    deleteButton.setAttribute('class', 'deleteButton');
+    deleteButton.setAttribute('id', `${idNumber}`);
+    deleteButton.setAttribute('onclick', 'deleteBook(this.id)');
+    deleteFeature.appendChild(deleteButton);
+    idNumber++;
+  }
+}
+
+function deleteBook(id) {
+  myLibrary.splice(id, 1);
+  render(myLibrary);
+}
+
+function status(check) {
+  var bookStatus = document.getElementById(check);
+  if (bookStatus.innerHTML == 'No') {
+    bookStatus.innerHTML = 'Yes';
+  } else {
+    bookStatus.innerHTML = 'No';
   }
 }
