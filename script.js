95c4367062f905e364d055534871d96fb87d1db3
Ben <ben10baek@hotmail.com>
2020-04-07 19:31:49 -0700

    Added local storage to library

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 let myLibrary = [];
+let myLibraryLocal = '';
 
 let library = document.getElementById('library');
 
 var form = document.getElementById('popupForm');
 var newBook = document.getElementById('addBook');
-var span = document.getElementsByClassName('form');
+var span = document.getElementsByClassName('close')[0];
 
 var idNumber = 0;
 
-const bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'No');
-const booktwo = new Book('Meditations', 'Marcus Aurelius', '200', 'Yes');
-const bookthree = new Book(
-  'Thinking Fast and Slow',
-  'Daniel Kahneman',
-  '400',
-  'No'
-);
+// const bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'No');
+// const booktwo = new Book('Meditations', 'Marcus Aurelius', '200', 'Yes');
+// const bookthree = new Book(
+//   'Thinking Fast and Slow',
+//   'Daniel Kahneman',
+//   '400',
+//   'No'
+// );
 
-addBookToLibrary(bookOne);
-addBookToLibrary(booktwo);
-addBookToLibrary(bookthree);
+// addBookToLibrary(bookOne);
+// addBookToLibrary(booktwo);
+// addBookToLibrary(bookthree);
 
-newBook.onclick = function() {
+newBook.onclick = function () {
   document.getElementById('title').value = null;
   document.getElementById('author').value = null;
   document.getElementById('pages').value = null;
@@ -30,11 +31,11 @@ newBook.onclick = function() {
   form.style.display = 'block';
 };
 
-span.onclick = function() {
+span.onclick = function () {
   form.style.display = 'none';
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == form) {
     this.form.style.display = 'none';
   }
@@ -72,15 +73,18 @@ function createNewBook() {
   book.read = read;
   //add book to temp array
   addBookToLibrary(book);
+  storeData();
   //tempLibrary.push(book);
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book); //pushing new book to library
-  render(myLibrary);
+  render();
+  storeData();
 }
 
 function render(library = myLibrary) {
+  idNumber = 0;
   if (library == '') {
     alert('ERROR\nPlease add a book before\nrendering.');
   }
@@ -93,14 +97,15 @@ function render(library = myLibrary) {
 }
 
 function makeTable(table, library) {
+  console.log(library);
   for (let book of library) {
     let row = table.insertRow();
-    row.setAttribute('id', `row${idNumber}`);
+    row.setAttribute('id', `${idNumber}`);
     for (key in book) {
       let cell = row.insertCell();
+      cell.setAttribute('id', `${key}${idNumber}`);
       if (key == 'read') {
-        cell.setAttribute('class', 'read');
-        cell.setAttribute('id', `read${idNumber}`);
+        cell.setAttribute('id', `row${idNumber}`);
         cell.setAttribute('onclick', 'status(this.id)');
       }
       let text = document.createTextNode(book[key]);
@@ -110,7 +115,6 @@ function makeTable(table, library) {
     var deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'Delete Book';
     deleteButton.setAttribute('class', 'deleteButton');
-    deleteButton.setAttribute('id', `${idNumber}`);
     deleteButton.setAttribute('onclick', 'deleteBook(this.id)');
     deleteFeature.appendChild(deleteButton);
     idNumber++;
@@ -119,14 +123,40 @@ function makeTable(table, library) {
 
 function deleteBook(id) {
   myLibrary.splice(id, 1);
-  render(myLibrary);
+  localStorage.removeItem(`libraryStorage: ${id}`);
+  render();
+  storeData();
 }
 
 function status(check) {
+  console.log(check);
   var bookStatus = document.getElementById(check);
+  currentBook = bookStatus.parentNode.firstChild.innerHTML;
+  let index = myLibrary.findIndex((Book) => Book.title == currentBook);
   if (bookStatus.innerHTML == 'No') {
     bookStatus.innerHTML = 'Yes';
+    myLibrary[index].read = 'Yes';
   } else {
     bookStatus.innerHTML = 'No';
+    myLibrary[index].read = 'No';
   }
+  storeData();
 }
+
+function storeData() {
+  localStorage.setItem('libraryStorage', JSON.stringify(myLibrary));
+}
+
+window.onload = function () {
+  if (localStorage.getItem('libraryStorage') == null) {
+    //empty storage
+    storeData();
+  } else {
+    myLibraryLocal = JSON.parse(localStorage.getItem('libraryStorage'));
+    this.localStorage.clear();
+    for (let i = 0; i < myLibraryLocal.length; i++) {
+      this.console.log('book: ' + i);
+      this.addBookToLibrary(myLibraryLocal[i]);
+    }
+  }
+};
